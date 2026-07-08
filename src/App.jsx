deb802bad3a5485be5cb8d2591f92e8bd9315e16
Sanket Chavan <sanketc2102@gmail.com>
2026-07-08 16:25:08 +0530

    impliment loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
+import Cart from "./features/cart/Cart";
 import OrderItem from "./features/order/OrderItem";
 import AppLayout from "./ui/AppLayout";
 
@@ -20,6 +21,12 @@ export default function App() {
           element: <Menu />,
           loader: menuLoader,
         },
+
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+
         {
           path: "/order/new",
           element: <CreateOrder />,
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { useFormState } from "react-dom";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
